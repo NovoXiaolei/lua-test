65a40dae2fd369b657c9bdb94c9d5bd88d381ccd
xiaolei <childmercy@163.com>
2017-01-09 19:03:39 +0800

    add ls_map function

--- c-api/common.h
+++ c-api/common.h
@@ -77,3 +77,19 @@ static int l_dir(lua_State *L){
     closedir(dir);
     return 1;
 }
+
+static int l_map(lua_State *L){
+    int i, n;
+    lua_checktype(L, 1, LUA_TABLE);
+    lua_checktype(L, 2, LUA_TFUNCTION);
+
+    //获得table的大小
+    n = lua_objlen(L, 1);
+    for(i = 1; i<=n;i++){
+        lua_pushvalue(L, 2);
+        lua_rawgeti(L, 1, i);
+        lua_call(L, 1, 1);
+        lua_rawseti(L, 1, i);
+    }
+    return 0;
+}
