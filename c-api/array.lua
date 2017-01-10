7ad3caf48eefa7ba141f6be149f6800035d6c09e
xiaolei <childmercy@163.com>
2017-01-10 18:43:13 +0800

    1

--- c-api/array.lua
+++ c-api/array.lua
@@ -4,10 +4,10 @@ local myarray = require "myarray"
 print(_G.package.cpath)
 hi = myarray.hello_c('hi c, from lua')
 print(hi)
---a = array.new(200)
---print(a)
---print(array.size(a));
+a = myarray.new(200)
+print(a)
+print(myarray.size(a));
 --for i = 1, 1000 do
---    array.set(a, i, i%5 ==0)
+--    myarray.set(a, i, i%5 ==0)
 --end
 --print(data.get(a, 10))
